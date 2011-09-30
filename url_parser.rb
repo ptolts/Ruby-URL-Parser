687553d6bcafd93e5831c58142fab935d4a09fde
Entrity <quinesquines@gmail.com>
2011-09-30 09:54:27 -0700

    Add 'Matcher' module so that query data (even if it includes lists) can be effectively compared.

--- url_parser.rb
+++ url_parser.rb
@@ -1,41 +1,44 @@
+require 'rubygems'
+require 'cgi'
+
 class Entrity
-	class URL
+	
+	module URL
+	
+		# Returns an instance of Parsed
+		def self.parse(url)
+			Parsed.new(url)
+		end
+		
+		# Returns true if 2 url Strings match. (Will parse strings and compare attributes.)
+		# ignore_nil == true: a nil value for an attribute in either url counts as a match
+		# Attempts to normalize any relative urls against absolute urls
+		def self.urls_identical?(url1, url2, ignore_nil=true, normalize=true)
+			p1 = parse(url1)
+			p2 = parse(url2)
+			Parsed.urls_identical?(p1, p2)
+		end
+	
 		class Parsed
-			
 			@@attrs = [:scheme, :host, :port, :path, :frag, :data]
 			attr_accessor *@@attrs
 			
+			def self.attrs; @@attrs; end
+			
 			# Returns an instance of Parser
 			def self.parse(url)
-				parser = Parsed.new(url)
-			end
-			
-			# Called by self.urls_identical?
-			# Returns true if two parsed urls have identical strings for given attribute
-			# - ignore_nil = [boolean] - a nil value for the attribute in either url counts as a match
-			def self._attr_match?(sym, parsed_url1, parsed_url2, ignore_nil)
-				attr1 = parsed_url1.send sym
-				attr2 = parsed_url2.send sym
-				if attr1 == attr2
-					true
-				elsif ignore_nil
-					attr1.nil? or attr2.nil?
-				else
-					false
-				end
+				self.new(url)
 			end
 			
 			# Returns true if two urls (not parsed) match
 			# - ignore_nil = [boolean] - a nil value for an attribute in either url counts as a match
 			# - normalize = [boolean] - if one path is relative but not both, then the relative path will be normalized as though it were relative to the absolute path
 			def self.urls_identical?(url1, url2, ignore_nil=true, normalize=true)
-				p1 = parse(url1)
-				p2 = parse(url2)
-				if normalize and (p1.relative? ^ p2.relative?)
-					p1.relative? ?	p2.normalize_path!(p1) : p1.normalize_path!(p2)
+				if normalize and (url1.relative? ^ url2.relative?)
+					url1.relative? ? url2.normalize_path!(url1) : url1.normalize_path!(url2)
 				end
 				@@attrs.each do |attr|
-					return false unless _attr_match?(attr, p1, p2, ignore_nil, normalize)
+					return false unless attr_match?(attr, url1, url2, ignore_nil)
 				end
 				true
 			end
@@ -104,7 +107,7 @@ class Entrity
 			def inspect(lf=true)
 				joiner = lf ? "\n" : " "
 				attrs = @@attrs.collect{|x| send x}
-				puts [*[to_s + '{'] + attrs + ['}']].join(joiner)
+				[*[to_s + '{'] + attrs + ['}']].join(joiner)
 			end
 
 			# Sets @scheme, @host, @port, @path, @data
@@ -131,8 +134,64 @@ class Entrity
 				frag		= /#([^?]*)/
 				regex 	=  /^(#{scheme}#{host})?#{port}?#{path}#{frag}?#{data}?/
 				regex.match(url.to_s)
-			end	
+			end
+			
+			# Called by self.urls_identical?
+			# Returns true if two parsed urls have identical strings for given attribute
+			# - ignore_nil = [boolean] - a nil value for the attribute in either url counts as a match
+			def self.attr_match?(sym, parsed_url1, parsed_url2, ignore_nil)
+				attr1 = parsed_url1.send sym
+				attr2 = parsed_url2.send sym
+				# Create Hashes if attribute is query data
+				if attr1 == attr2
+					true
+				elsif ignore_nil and (attr1.nil? or attr2.nil?)
+					true
+				elsif sym == :data
+					attr1 = CGI::parse attr1
+					attr2 = CGI::parse attr2
+					if attr1 == attr2
+						true
+					else
+						Entrity::Matcher::hashes_match?(attr1, attr2)
+					end
+				else
+					false
+				end
+			end
 			
 		end
+		
 	end
+	
+	module Matcher
+		
+		# Returns true if hashes have all the k-v pairs (w/ arrays matching regardless of order)
+		def self.hashes_match?(h1, h2)
+			return false if h1.length != h2.length
+			h1.each_key do |key|
+				return false if !elements_match?(h1[key], h2[key])
+			end
+			true
+		end
+
+		# Returns true if arrays have all the same elements, regardless of order
+		def self.arrays_match?(a1, a2)
+			return false if a1.length != a2.length
+			a1.each do |e1|
+				return false if a2.all? { |e2| !elements_match?(e1, e2) }
+			end
+			true
+		end
+
+		# Returns true if elements match (kind of)
+		def self.elements_match?(e1, e2)
+			return false if e1.class != e2.class
+			return hashes_match?(e1, e2) if e1.is_a?(Hash)
+			return arrays_match?(e1, e2) if e1.is_a?(Array)
+			return e1 == e2
+		end
+
+	end	
+	
 end
